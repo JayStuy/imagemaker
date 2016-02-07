514e503db505a49832273a8ab1e4b41220e104b6
Jay Froment-Rudder <jfromentrudder@gmail.com>
2016-02-07 17:50:36 -0500

    finished

--- imagemaker.c
+++ imagemaker.c
@@ -7,21 +7,19 @@ int main() {
   FILE* f;
   f = fopen("image.ppm", "a");
 
-  fprintf(f, "P3 650 650 255\n");
+  fprintf(f, "P3 500 500 255 ");
   
-  int R, G, B;
-  int x = 0;
-  int a, b, c;
+  int R, G, B = 0;
+  int a, b;
 
-  while(x < 50) {
-    a = rand();
-    b = rand();
-    c = rand();
-    R = (a^3+b^2+c)%256;
-    G = (a^2+b^2)%256;
-    B = (a^3+b^4+c^2)%256;
+  for(a = 0; a < 500; a ++) {
+    for(b = 0; b < 500; b ++) {
+      R = (a*a+b*b)%256;
+      G = (a*a+b)%256;
+      B = (a+b*b+5)%256;
 
-    fprintf(f, "%d %d %d", R, G, B);
-    x ++;
+      fprintf(f, "%i %i %i ", R, G, B);
+    }
   }
+  fclose(f);
 }
